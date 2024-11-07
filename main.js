295015a4efec19cc560e7fb68bebf0e8b5300538
cbyzz24 <155403160+cbyzz24@users.noreply.github.com>
2024-11-07 13:14:46 -0700

    Total Bill display added

--- main.js
+++ main.js
@@ -3,15 +3,19 @@ const tipPct = document.getElementById("tipPct");
 const calculate = document.getElementById("calculate");
 const reset = document.getElementById("reset");
 const resultDisplay = document.getElementById("resultDisplay");
+const totalAmtDisplay = document.getElementById("totalAmtDisplay");
 
 
 function calculateTip(){
     tipAmt = (billAmt.value * (tipPct.value/100));
     resultDisplay.innerText = tipAmt.toLocaleString("en-US", {style: "currency", currency: "USD"});
+    billTotal = (billAmt.value * (1+(tipPct.value/100)));
+    totalAmtDisplay.innerText = billTotal.toLocaleString("en-US", {style: "currency", currency: "USD"});
 }
 
 function clearEntries(){
     billAmt.value = "$0.00";
     tipPct.value = "0.00%";
     resultDisplay.innerText = "$0.00";
+    totalAmtDisplay.innerText = "$0.00";
 }
